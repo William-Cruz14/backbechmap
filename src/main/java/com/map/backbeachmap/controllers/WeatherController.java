286fd4dd30f5b514d442a61182115400463852f4
William Cruz <cruzwilliam14@gmail.com>
2024-11-25 10:29:47 -0300

    Configuring controller weather

--- src/main/java/com/map/backbeachmap/controllers/WeatherController.java
+++ src/main/java/com/map/backbeachmap/controllers/WeatherController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping("/api")
+@RequestMapping("/weather")
 public class WeatherController {
 
     private final WeatherService weatherService;
