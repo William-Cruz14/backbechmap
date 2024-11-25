dea0e294d3babbc92aa6436b29fca39c21b1199e
William Cruz <cruzwilliam14@gmail.com>
2024-11-25 10:38:14 -0300

    Editing Endpoint

--- src/main/java/com/map/backbeachmap/controllers/GeocodingController.java
+++ src/main/java/com/map/backbeachmap/controllers/GeocodingController.java
@@ -3,13 +3,11 @@ package com.map.backbeachmap.controllers;
 import com.map.backbeachmap.services.GeocodingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
+@RequestMapping("api")
 public class GeocodingController {
     @Autowired
     private GeocodingService geocodingService;
--- src/main/java/com/map/backbeachmap/controllers/WeatherController.java
+++ src/main/java/com/map/backbeachmap/controllers/WeatherController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping("/weather")
+@RequestMapping("api")
 public class WeatherController {
 
     private final WeatherService weatherService;
